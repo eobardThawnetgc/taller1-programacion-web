fa442c68c9ed7059b23764ab34e7ed1533eeca09
dgonzag <00072825@uca.edu.sv>
2026-08-31 15:06:07 -0600

    feat: Elaboracion de ej3 y ej4

--- taller1.js
+++ taller1.js
@@ -40,6 +40,59 @@ function clasificarNota(nota) {
 let notaEstudiante = 85;
 console.log(`Nota: ${notaEstudiante} -> Clasificación: ${clasificarNota(notaEstudiante)}`);
 
+//Ejercicio 3
+function AFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+let valorEntrada = Number(prompt("Digite el numero en celsius"));
+//Ejemplo de uso:
+let valorSalida = AFahrenheit(valorEntrada);
+
+console.log(valorEntrada + 'ºC = ' + valorSalida + 'ºF');
+
+//Ejercio 4
+//Calculadora con diferentes operaciones
+function calculadora(numero1, numero2, operacion) {
+  if (typeof numero1 !== 'number' || isNaN(numero1) || typeof numero2 !== 'number' || isNaN(numero2)) {
+    console.log("Debes ingresar numeros validos.");
+    return;
+  }
+
+  switch (operacion) {
+    case 1:
+      //Sumar respectivos numeros
+      console.log("El resultado de la operacion es: " + (numero1 + numero2));
+      break;
+
+    case 2:
+      //Restar respectivos numeros
+      console.log("El resultado de la operacion es: " + (numero1 - numero2));
+      break;
+
+    case 3:
+      //Multiplicar dichos numeros
+      console.log("El resultado de la operacion es: " + (numero1 * numero2));
+      break;
+
+    case 4:
+      //Dividir respectivos numeros
+      if (numero2 === 0) {
+        console.log("No se puede dividir entre cero.");
+      } else {
+        console.log("El resultado de la operación es: " + (numero1 / numero2));
+      }
+      break;
+
+    default:
+      console.log("Opción no permitida. Intentelo de nuevo.");
+  }
+}
+
+let operacion = Number(prompt("Que desea hacer?\n1.Sumar \n2.Restar \n3.Multiplicacion \n4.Division"));
+let numero1 = Number(prompt("Digite el primer numero"));
+let numero2 = Number(prompt("Digite el segundo numero"));
+calculadora(numero1, numero2, operacion);
 
 //ejercicio 5: Generador de usuario 
 const generarUsuario = (nombre) => {
@@ -64,4 +117,3 @@ const producto2 = new Producto("Hielera", 10, 85);
 console.log("Productos creados:");
 console.log(producto1);
 console.log(producto2);
-
